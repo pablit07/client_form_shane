184faee66db8f4d6dfbebd64173caa96acafbf58
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-04-09 13:31:45 -0600

    work on interface including knockout saving and loading

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@
 </head>
 <body>
 
+<?php 
+  if ($_SERVER['PHP_AUTH_USER'] != 'thecrapscoach' || $_SERVER['PHP_AUTH_PW'] != 'crapscoach') {
+    header('WWW-Authenticate: Basic realm="Dice Roll Recorder"');
+    header('HTTP/1.0 401 Unauthorized');
+    exit;
+  }
+?>
+
 <!-- FONT AWESOME -->
 <script src="https://use.fontawesome.com/370aaf74a2.js"></script>
 
@@ -25,6 +33,10 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min.js"></script>
 
 <style type="text/css">
+header.navbar {
+  padding-left: 10px;
+  background-color: #FFFFCC;
+}
 .back, .next, .submit {
     display: inline-block;
   	width: 100% !important;  
@@ -73,11 +85,11 @@
     filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#49B3E8', endColorstr='#1591D0') !important;
 }
 </style>
+<header class="navbar navbar-light"><h2 class="viewport_title"><i class="fa fa-braille"></i> Craps Roll Recorder <i class="fa fa-braille"></i></h2></header>
+<div class="container">
 
 <form id="sellerform" class="form form_wrap_light" data-toggle="validator" role="form" name="seller_form" method="post" action="/home-value-api/" autocomplete="off">
 <!-- class="inbound-now-form wpl-track-me inbound-track"-->
-
-<h2 class="viewport_title"><i class="fa fa-braille"></i> Craps Roll Recorder</h2>
 
 
   <div class="step">
@@ -88,13 +100,30 @@
           <div class="input-group">                
             <div class="input-group-addon"><span class="fa fa-video-camera"></span></div>
 
-            <input type="text" class="form-control" id="first_name" name="first_name" data-map-form-field="wpleads_first_name" required data-required="true" maxlength="32"/>            
+            <input type="text" class="form-control" id="ip-address" name="ipAddress" required data-required="true" maxlength="32" data-bind='value: ipAddress'/>            
                                         
           </div>
         </div>
       </div>
     </div>
   </div>
+
+
+   <div class="step">
+    <h3>Google Sheets URL</h3>        
+    <div class="row gutter-10">
+      <div class="col-lg-12">
+        <div class="form-group">
+          <div class="input-group">   
+            <div class="input-group-addon"><span class="fa fa-google"></span></div>
+
+            <input type="text" class="form-control" id="spreadsheet-url" name="spreadsheetUrl" required data-required="true" data-bind='value: spreadsheetUrl'/>       
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
    <div class="step">
     <h3>Camera Connection</h3>        
@@ -103,12 +132,12 @@
 
         <ul class="client_type">          
           <li>
-            <input type="radio" value="BUYER" name="amount" checked="checked" />
-            <label for="BUYER">On</label>
+            <input id='camera-state-on' type="radio" value="1" name="cameraState" data-bind="checked:cameraState, checkedValue: '1'"/>
+            <label for="camera-state-on">On</label>
           </li>
           <li>
-            <input type="radio" value="SELLER" name="amount" />
-            <label for="SELLER">Off</label>
+            <input id='camera-state-off' type="radio" value="0" name="cameraState" data-bind="checked:cameraState, checkedValue: '0'"/>
+            <label for="camera-state-off">Off</label>
           </li>
         </ul>
 
@@ -129,13 +158,44 @@
 
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 
-      <button type="submit" class="action submit" id="submit" name="zillow_submit">Save &nbsp;<i class="fa fa-angle-double-right"></i></button>
+      <button type="button" class="action submit" id="submit" name="submit" data-bind='click: save'>Save &nbsp;<i class="fa fa-angle-double-right"></i></button>
 
       </div>
 
   </div>
 
 </form>
+</div>
 
+<script type="text/javascript">
+  var SettingsViewModel = function() {
+    var self = this;
+
+    this.ipAddress = ko.observable('');
+    this.spreadsheetUrl = ko.observable('');
+    this.cameraState = ko.observable('0');
+
+    this.load = function(setInitialState) {
+      $.get('../api/settings', function(data) {
+        self.ipAddress(data.ip_address);
+        self.cameraState(data.camera_state);
+      });
+    }
+
+    this.load(true);
+
+    this.save = function() {
+      $.post('../api/settings', {
+        'ip_address': self.ipAddress(),
+        'camera_state': self.cameraState()
+      });
+    }
+    
+  };
+
+  $(function() {
+    ko.applyBindings(new SettingsViewModel());
+  });
+</script>
 </body>
 </html>
